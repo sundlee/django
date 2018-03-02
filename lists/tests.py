f6c2df73d217f95ef53aa4b623a0bbe1ed9992da
Yong-Hae Lee <sundlee@gmail.com>
2018-03-02 09:41:35 +0900

    테스트일때는 csrf관련 line을 삭제하도록 수정 + 테스트 케이스에서 두번째 아이템 추가

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,14 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+import re
 
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -15,3 +21,24 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertTrue(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html1 = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'},
+        )
+        expected_html2 = response.content.decode()
+        # print("response.content.decode(): 1111111111111111")
+        # print(expected_html2)
+        # print("render_to_string: 2222222222222222")
+        # print(self.remove_csrf(expected_html1))
+
+
+        self.assertEqual(self.remove_csrf(expected_html2), self.remove_csrf(expected_html1))
