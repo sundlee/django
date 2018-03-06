1afbd8924a62e01837a604cfbbcf335aaff05a4c
Yong-Hae Lee <sundlee@gmail.com>
2018-03-06 21:07:11 +0900

    functional_tests 앱 생성 및 LiveServerTestCase 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,8 @@
-from contextlib import contextmanager
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import staleness_of
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
-
-    # @contextmanager
-    # def wait_for_page_load(self, timeout=30):
-    #     old_page = self.browser.find_element_by_tag_name('html')
-    #     yield
-    #     WebDriverWait(self.browser, timeout).until(staleness_of(old_page))
-
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -36,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스 (Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
         self.assertIn('To-Do', self.browser.title)
@@ -57,9 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다.
         inputbox.send_keys(Keys.ENTER)
-        # self.browser.implicitly_wait(3)
 
-#        with self.wait_for_page_load(timeout=10):
         self.check_for_row_in_list_table("1: 공작깃털 사기")
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
@@ -70,7 +58,6 @@ class NewVisitorTest(unittest.TestCase):
         # self.browser.implicitly_wait(3)
 
         # 페이지는 다시 갱신되고 두 개 아이템이 목록에 보인다
- #       with self.wait_for_page_load(timeout=10):
         self.check_for_row_in_list_table("1: 공작깃털 사기")
         self.check_for_row_in_list_table("2: 공작깃털을 이용해서 그물 만들기")
 
@@ -82,6 +69,5 @@ class NewVisitorTest(unittest.TestCase):
 
         # 만족하고 잠자리에 든다.
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+
 
