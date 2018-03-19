d7e0cadd8f0caf138a936c0f46878b0ed9e9a600
Yong-Hae Lee <sundlee@gmail.com>
2018-03-19 22:00:40 +0900

    기존 리스트에 아이템을 추가하기 위한 신규 URL 및 뷰, FT성공

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -86,7 +86,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('공작깃털 사기')
+        self.assertNotIn('공작깃털 사기', page_text)
         self.assertIn('우유 사기', page_text)
 
         # 둘 다 만족하고 잠자리에 든다
