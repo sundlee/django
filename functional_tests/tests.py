40a1a56e44a195ce08ff29ecab36aa6f08566318
Yong-Hae Lee <sundlee@gmail.com>
2018-03-25 13:07:20 +0900

    부트스트랩 추가와 레이아웃 개선

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,20 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
